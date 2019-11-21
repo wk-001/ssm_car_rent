262ce54aaeeb08a399a71f2e81d7dece375c5db8
wk-001 <2326699650@qq.com>
2019-11-21 17:48:43 +0800

    生成普通、带图片、带图片和文字的二维码

--- src/test/com/wk/NormalTest.java
+++ src/test/com/wk/NormalTest.java
@@ -1,19 +1,34 @@
 package com.wk;
 
+import com.wk.sys.utils.ZXingCodeUtil;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 
 public class NormalTest {
 
 	@Test
-	public void numberTest(){
-		List<Double> sale = new ArrayList<>(12);
-		for (int i = 3; i <= 12; i++) {
-			sale.add(i,0.0);
-		}
-		System.out.println("sale = " + sale);
+	public void qrcodeTest() throws IOException {
+		String qrUrl = "http://www.bilibili.com";
+		String destFile = "D:\\qrcode\\" + new Date().getTime() + ".png";
+		String logoPath = "D:\\qrcode\\cat.jpg";
+		String text = "哔哩哔哩";
+
+		//生成普通二维码，文件夹必须存在
+		BufferedImage bim = ZXingCodeUtil.createCode(qrUrl);
+		/*ImageIO.write(bim, "png", new File(destFile));
+
+		bim = ZXingCodeUtil.createCodeWithLogo(qrUrl,logoPath);
+		ImageIO.write(bim, "png", new File(destFile));*/
+
+		bim = ZXingCodeUtil.createCodeWithLogoAndText(qrUrl,logoPath,text);
+		ImageIO.write(bim, "png", new File(destFile));
+
+		System.out.println("done");
 	}
 
 }
